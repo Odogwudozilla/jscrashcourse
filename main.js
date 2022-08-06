ed27f6dd92e95f1718ca3dfe41bd54117a56bd42
Chidozie Nnachor <Chidozie.Nnachor@keylane.com>
2022-08-07 00:39:16 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,9 @@
+const name = 'John';
+const age = 30;
+const rating = 4.5;
+const istrue = true;
+const x = null;
+const y = undefined;
+let z;
+
+console.log(`My name is ${name} and I am ${age} years old`);
